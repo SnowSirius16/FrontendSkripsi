a7ba2f1347ca97e2b7f8f60520a8354ceb63f3c2
SnowSirius16 <syahida.asa.16@gmail.com>
2023-07-24 14:51:39 +0700

    Perbaikan halaman yang tertukar

--- app/src/main/java/com/example/frontendskripsi/catatanTertambah.java
+++ app/src/main/java/com/example/frontendskripsi/catatanTertambah.java
@@ -2,7 +2,11 @@ package com.example.frontendskripsi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class catatanTertambah extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class catatanTertambah extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catatan_tertambah);
+
+        TextView tv = findViewById(R.id.tvJudul);
+        ImageButton ib = findViewById(R.id.ibLainnya);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(catatanTertambah.this, HalamanCatatan.class);
+                startActivity(it);
+            }
+        });
     }
 }
